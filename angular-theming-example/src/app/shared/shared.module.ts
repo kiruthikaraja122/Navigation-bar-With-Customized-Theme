e2899d2d0800dc5cf534263e3fa41540d63b67bc
kiruthikaraja122 <kirthiraja444@gmail.com>
2022-12-22 17:38:30 +0530

    code added

---
+++ angular-theming-example/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//import {CustomMaterialModule} from '''./custom-material/custom-material.module';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidenavComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
